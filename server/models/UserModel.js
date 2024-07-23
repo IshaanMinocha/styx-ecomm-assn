6d4ebbe3e928ae0f8e375105465e3a8ffced2374
Ishaan Minocha <minochaishaan2003@gmail.com>
2024-07-23 08:00:41 +0530

    chore(server): added orders to user model

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -24,6 +24,12 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
+    orders: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Order'
+        }
+    ],
 },
     {
         collection: "User",
