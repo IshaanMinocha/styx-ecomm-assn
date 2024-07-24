97e37c0f6bc18d6b81d631e59b26def02fdf3384
Ishaan Minocha <minochaishaan2003@gmail.com>
2024-07-24 07:20:33 +0530

    fix: added builder object

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -8,39 +8,38 @@ export const cartReducer = createReducer(
     tax: 0,
     total: 0,
   },
-  {
-    addToCart: (state, action) => {
-      const item = action.payload;
-      const isItemExist = state.cartItems.find((i) => i.id === item.id);
+  (builder) => {
+    builder
+      .addCase('addToCart', (state, action) => {
+        const item = action.payload;
+        const isItemExist = state.cartItems.find((i) => i.id === item.id);
 
-      if (isItemExist) {
-        state.cartItems.forEach((i) => {
-          if (i.id === item.id) i.quantity += 1;
-        });
-      } else {
-        state.cartItems.push(item);
-      }
-    },
-
-    decrement: (state, action) => {
-      const item = state.cartItems.find((i) => i.id === action.payload);
-      if (item.quantity > 1) {
-        state.cartItems.forEach((i) => {
-          if (i.id === item.id) i.quantity -= 1;
-        });
-      }
-    },
-
-    deleteFromCart: (state, action) => {
-      state.cartItems = state.cartItems.filter((i) => i.id !== action.payload);
-    },
-    calculatePrice: (state) => {
-      let sum = 0;
-      state.cartItems.forEach((i) => (sum += i.price * i.quantity));
-      state.subTotal = sum;
-      state.shipping = state.subTotal > 1000 ? 0 : 200;
-      state.tax = +(state.subTotal * 0.18).toFixed();
-      state.total = state.subTotal + state.tax + state.shipping;
-    },
+        if (isItemExist) {
+          state.cartItems.forEach((i) => {
+            if (i.id === item.id) i.quantity += 1;
+          });
+        } else {
+          state.cartItems.push(item);
+        }
+      })
+      .addCase('decrement', (state, action) => {
+        const item = state.cartItems.find((i) => i.id === action.payload);
+        if (item.quantity > 1) {
+          state.cartItems.forEach((i) => {
+            if (i.id === item.id) i.quantity -= 1;
+          });
+        }
+      })
+      .addCase('deleteFromCart', (state, action) => {
+        state.cartItems = state.cartItems.filter((i) => i.id !== action.payload);
+      })
+      .addCase('calculatePrice', (state) => {
+        let sum = 0;
+        state.cartItems.forEach((i) => (sum += i.price * i.quantity));
+        state.subTotal = sum;
+        state.shipping = state.subTotal > 1000 ? 0 : 200;
+        state.tax = +(state.subTotal * 0.18).toFixed();
+        state.total = state.subTotal + state.tax + state.shipping;
+      });
   }
 );
